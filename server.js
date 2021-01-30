23775a54537f71b66e6bea2d9b81e0d09916eb1b
Nishat Hoque <hoquen0397@gmail.com>
2021-01-30 17:59:28 -0500

    authorization

--- server.js
+++ server.js
@@ -1,15 +1,36 @@
 /* eslint-disable quotes */
 /* eslint-disable indent */
 'use strict';
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 
 const app = express();
 app.use(morgan('dev'));
 
-app.use((req,res) => {
-    res.send ("Hello");
+console.log(process.env.API_TOKEN);
+
+app.use(function validateBearerToken(req,res,next){
+    const apiToken = process.env.API_TOKEN;
+    const authToken = req.get('Authorization')
+    if (!authToken || authToken.split(' ')[1] !== apiToken){
+        return res.status(401).json({ error: 'Unauthorized request' });
+    }
+    next();
 });
+
+const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`];
+function handleGetTypes(req,res){
+    res.json(validTypes);
+}
+
+app.get('/types', handleGetTypes);
+
+function handleGetPokemon(req,res){
+    res.send('Hello,Pokemon!');
+}
+
+app.get('/pokemon',handleGetPokemon);
 
 const PORT = 8080;
 
