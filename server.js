364fa866a014b94e16d2e9182d4c25eef31ec486
Nishat Hoque <hoquen0397@gmail.com>
2021-02-03 11:57:42 -0500

    revised for deployment

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ const cors = require('cors');
 const POKEDEX =require('./POKEDEX.json');
 
 const app = express();
-app.use(morgan('dev'));
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common';
+
+app.use(morgan(morganSetting));
 app.use(helmet());
 app.use(cors());
 
@@ -53,8 +55,17 @@ function handleGetPokemon(req,res){
 
 app.get('/pokemon',handleGetPokemon);
 
-const PORT = 8080;
-
-app.listen(PORT, () => {
-    console.log(`Server listening at port ${PORT}`);
+app.use((error, req, res, next) => {
+    let response;
+    if (process.env.NODE_ENV === 'production') {
+      response = { error: { message: 'server error' }};
+    } else {
+      response = { error };
+    }
+    res.status(500).json(response);
 });
+  
+
+const PORT = process.env.PORT||8080;
+
+app.listen(PORT);
